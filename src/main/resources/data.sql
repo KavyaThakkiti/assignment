127683736a2f8d499d43a856e05c3ff4dbf9e740
KavyaThakkiti <100115314+KavyaThakkiti@users.noreply.github.com>
2022-03-04 15:49:32 +0530

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,25 @@
+insert into employee(id, name, last_name) 
+values(100,'sowmya','siri');
+insert into employee (id, name, last_name) 
+values(101,'kavya' ,'bandaru' );
+insert into employee(id, name, last_name ) 
+values(102,'sagar', 'chandhra');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
